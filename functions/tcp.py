5e8854731a5dc65091940c4996c7ea7a4078e47f
glenlancer <glenlancer@163.com>
2020-02-10 23:11:27 +0800

    Some progress in tcp.py

--- functions/tcp.py
+++ functions/tcp.py
@@ -5,16 +5,53 @@ class Tcp(object):
     def __init__(self, config):
         self.config = config
         self.socket_ref = None
-        self.ai_family = socket.AF_UNSPEC
+        self.ai_family = self.config.ai_family
+
+    def connect(self, hostname, port, is_secure, local_if=None):
+        try:
+            gai_results = socket.getaddrinfo(
+                host,
+                port,
+                self.ai_family,
+                socket.SOCK_STREAM,
+                socket.IPPROTO_TCP,
+                socket.AI_PASSIVE
+            )
+        except socket.gaierror as e:
+            Tcp.print_error(hostname, port, e.message)
+            return False
+        for gai_result in gai_results:
+            ai_family, ai_sockettype, ai_protocol, _, ai_addr = gai_result
+            try:
+                self.close()
+                self.socket_ref = socket.socket(
+                    ai_family,
+                    socket_type,
+                    ai_protocol
+                )
+                # Does this mean the port number is dynamic allocated?
+                if local_if is not None and ai_family == socket.AF_INET:
+                    self.socket_ref.bind((local_if, 0))
+                self.socket_ref.setsockopt(socket.IPPROTO_TCP, socket.TCP_FASTOPEN, None, 0)
+                self.socket_ref.connect(ai_addr)
+                # https://www.cnblogs.com/zhiyong-ITNote/p/7553694.html Check this !!!
+            except socket.error as e:
+                Tcp.print_error(hostname, port, e.message)
+                return False
+
+    def close(self):
+        if self.socket_ref is not None:
+            self.socket_ref.close()
+            self.socket_ref = None
+
+    def read(self):
         pass
 
-    def connect(self, hostname, port, is_secure, local_if, io_timeout):
-        local_addr = {
-            'sin_family': socket.AF_INET,
-            'sin_port': 0,
-            'sin_addr': local_if,
-        }
-        # addr_info = socket.getaddrinfo(hostname, port, )
+    def write(self):
+        pass
+
+    def get_interface_ip(self):
+        pass
 
     @staticmethod
     def print_error(hostname, port, reason):
--- functions/temp.py
+++ functions/temp.py
@@ -1,7 +1,17 @@
 import socket
 import sys
 
-host = 'daring.cwi.nl'
-port = 5007
+host = 'www.baidu.com'
+port = 80
 s = None
 
+res = socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_STREAM, 0, socket.AI_PASSIVE)[0]
+print(res)
+'''
+af, sockettype, proto, canonname, sa = res[0]
+print('af:', af)
+print('sockettype', sockettype)
+print('proto', proto)
+print('cannonname', canonname)
+print('sa', sa)
+'''

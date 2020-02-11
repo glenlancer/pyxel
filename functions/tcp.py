6c87f54bc75d9cb10af8af8981950b135df5b2a6
glenlancer <glenlancer@163.com>
2020-02-11 13:54:43 +0800

    Skeleton buildup. 4 - 2/11/2020 tcp.c

--- functions/tcp.py
+++ functions/tcp.py
@@ -7,7 +7,7 @@ class Tcp(object):
         self.socket_ref = None
         self.ai_family = self.config.ai_family
 
-    def connect(self, hostname, port, is_secure, local_if=None):
+    def connect(self, hostname, port, is_secure, io_timeout, local_if=None):
         try:
             gai_results = socket.getaddrinfo(
                 host,
@@ -34,23 +34,33 @@ class Tcp(object):
                     self.socket_ref.bind((local_if, 0))
                 self.socket_ref.setsockopt(socket.IPPROTO_TCP, socket.TCP_FASTOPEN, None, 0)
                 self.socket_ref.connect(ai_addr)
-                # https://www.cnblogs.com/zhiyong-ITNote/p/7553694.html Check this !!!
+                # select.select?
             except socket.error as e:
                 Tcp.print_error(hostname, port, e.message)
-                return False
+            if self.socket_ref is not None:
+                break
+        if self.socket_ref is None:
+            return False
+        self.socket_ref.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, io_timeout)
+        self.socket_ref.setsockopt(socket.SOL_SOCKET, socket.SO_SNDTIMEO, io_timeout)
+        return True
 
     def close(self):
         if self.socket_ref is not None:
             self.socket_ref.close()
             self.socket_ref = None
 
-    def read(self):
-        pass
+    def read(self, recv_size):
+        if self.socket_ref is None:
+            raise Exception(f'Exception in {__name__}: socket_ref is None.')
+        return self.socket_ref.recv(recv_size)
 
-    def write(self):
-        pass
+    def write(self, send_size):
+        if self.socket_ref is None:
+            raise Exception(f'Exception in {__name__}: socket_ref is None.')
+        return self.socket_ref.send(send_size)
 
-    def get_interface_ip(self):
+    def get_interface_ip(self, interface):
         pass
 
     @staticmethod
